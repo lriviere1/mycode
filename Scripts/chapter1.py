cf3f1891997f00882dfc27b995549765c96e98bd
Test <example@example.com>
2026-03-30 06:30:40 -0400

    Chapter 1 and 2 Python homework

---
+++ Scripts/chapter1.py
@@ -0,0 +1,4 @@
+print("Welcome to my first Python program!")
+print("My name is Labady.")
+print("I am learning Python programming.")
+print("This is Chapter 1 assignment.")
---
+++ Scripts/chapter2.py
@@ -0,0 +1,11 @@
+print("=== User Information Program ===")
+
+name = input("Labady: ")
+age = int(input("41: "))
+
+print("\nHello", name)
+
+if age >= 18:
+    print("You are an adult.")
+else:
+    print("You are under 18.")
